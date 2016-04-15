5f724ff00f478d0599a2bfdf9a27c4b6df89f07e
Diego Piedra <diego.piedraaraya@ucr.ac.cr>
2016-04-15 00:42:13 -0600

    Se agrega un acordeón para las opciones de los trabajos y se cambia el color del titulo

--- app/components/works/worksController.js
+++ app/components/works/worksController.js
@@ -2,7 +2,6 @@ app.controller('worksController', ['$scope', '$http', function($scope, $http) {
 	$scope.yearsRank = {
 	    start: [2000, 2015],
 	    step: 1,
-	    margin: 1,
 	    connect: true,
 	    behaviour: 'tap-drag',
 	    range: { 'min': 1995, 'max': 2016 },
